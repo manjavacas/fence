2e3f91f7d41dd4841cb59f2a7840ac22d31a8fe7
Antonio <Antonio@Antonio>
2020-02-06 21:08:22 +0100

    [src] First steps with String

---
+++ fence/src/main/java/com/manjavacas/fence/FenceApp.java
@@ -0,0 +1,13 @@
+package com.manjavacas.fence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenceApp.class, args);
+	}
+
+}
---
+++ fence/src/main/java/com/manjavacas/fence/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.manjavacas.fence.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "hi";
+	}
+	
+	
+}
