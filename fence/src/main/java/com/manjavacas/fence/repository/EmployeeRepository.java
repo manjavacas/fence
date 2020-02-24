7b5e845da66eca27a982d2e838b4c545dcb7039e
Antonio <Antonio@Antonio>
2020-02-24 12:09:14 +0100

    [src] Packages prepared

--- fence/src/main/java/com/manjavacas/fence/repository/EmployeeRepository.java
+++ fence/src/main/java/com/manjavacas/fence/repository/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.manjavacas.fence.repository;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.DeleteQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.manjavacas.fence.model.Employee;
@@ -12,13 +10,10 @@ import com.manjavacas.fence.model.Employee;
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 
-	@Query("{ 'dni' : ?0 }")
 	Employee findByDni(String dni);
 
-	@Query("{ 'team' : ?0 }")
 	List<Employee> findByTeam(String team);
 
-	@DeleteQuery("{ 'dni' : ?0}")
 	List<Employee> deleteByDni(String dni);
 
 	Employee findBy_id(String string);
